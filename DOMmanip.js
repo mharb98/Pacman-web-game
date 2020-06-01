7d7ecbf9f846d198910a011b209389d2c975e2fd
marwan harb <marwanharb65@outlook.com>
2020-06-01 12:50:07 +0200

    Cleaned code

--- DOMmanip.js
+++ DOMmanip.js
@@ -16,3 +16,39 @@ function renderTable(sudoku){
     }
     return ret;
 }
+
+function play(tdlist,numbers,colors,actual){
+    let activeCell = tdlist[0];
+    let row,column;
+    let n;
+    for(let i=0;i<tdlist.length;i++){
+        tdlist[i].addEventListener('click',(event)=>{
+            activeCell.style.backgroundColor = "#fbf3d4";
+            activeCell = tdlist[i];
+            activeCell.style.backgroundColor = "#89cff0";
+            row = tdlist[i].parentElement.rowIndex;
+            column = tdlist[i].cellIndex;
+        });
+    }
+    for(let i=0;i<numbers.length;i++){
+        numbers[i].addEventListener('click',(event)=>{
+                n = numbers[i].innerText;
+                if(colors[row][column]=="black"){
+                    alert("can't change that");
+                }
+                else{
+                    activeCell.innerText = n;
+                    if(n==actual[row][column]){
+                        console.log(actual[row][column]);
+                        activeCell.style.color = "green";
+                        colors[row][column] = "green";
+                    }
+                    else{
+                        activeCell.style.color = "red";
+                        colors[row][column] = "red";
+                        console.log(actual[row][column]);
+                    }
+                }
+        });
+    }
+}
