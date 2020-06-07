27ec8bdf9d333214212a3f72b867e162c96dc6c0
marwan harb <marwanharb65@outlook.com>
2020-06-07 13:37:25 +0200

    Added the walls and fixed bugs

--- PackmanGenerator.js
+++ PackmanGenerator.js
@@ -12,7 +12,9 @@ function createInitial(){
 }
 
 function getRandom(num1,num2){
-
+    let ret1 = Math.floor(Math.random()*num1)+2;
+    let ret2 = Math.floor(Math.random()*num2)+2;
+    return [ret1,ret2];
 }
 
 function editBorders(matrix){
@@ -30,11 +32,32 @@ function editBorders(matrix){
     }
 }
 
+function addWalls(matrix){
+    let randoms,ret1,ret2;
+    for(let i=0;i<5;i++){
+        randoms = getRandom(10,35);
+        ret1 = randoms[0];
+        ret2 = randoms[1];
+        for(let j=ret1;j<ret1+4;j++){
+            matrix[j][ret2]=2;
+        }
+    }
+    for(let i=0;i<5;i++){
+        randoms = getRandom(10,35);
+        ret1 = randoms[0];
+        ret2 = randoms[1];
+        for(let j=ret2;j<ret2+4;j++){
+            matrix[ret1][j]=2;
+        }
+    }
+}
+
 function generatePackman(){
     let k = createInitial();
     editBorders(k);
+    addWalls(k);
     k[7][20]=3;
-    k[1][1]=4;
-    k[1][39]=4;
+    k[1][3]=4;
+    k[1][15]=4;
     return k;
 }
